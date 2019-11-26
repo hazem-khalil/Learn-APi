42027cd5eb5954dd94ce038fdcd366b247e4b5c3
Hazem Khalil <hazem.ibrahim.khalil@gmail.com>
2019-11-26 22:34:58 +0200

    Making Update

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Person;
+use Illuminate\Http\Request;
 use App\Http\Requests\PersonRequest;
 use App\Http\Resources\PersonResource;
 use App\Http\Resources\PersonResourceCollection;
@@ -29,11 +30,23 @@ class PeopleController extends Controller
 
     /**
      * @param  Person  $person
-     * @return PersonResourceCollection
+     * @return PersonResource
      */
-    public function store(PersonRequest $request)
+    public function store(PersonRequest $request): PersonResource
     {
         $person = Person::create(request()->all());
         return new PersonResource($person);
+    }
+
+    /**
+     * assuming the user will input valid data until now
+     * @param  Person  $person
+     * @return PersonResource
+     */
+    public function update(Person $person, Request $request): PersonResource
+    {
+        $person->update($request->all());
+        return new PersonResource($person);
+        // return new PersonResource($person);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,6 @@
 use Illuminate\Http\Request;
 
 Route::get('/people/', 'PeopleController@index');
-Route::post('/people/', 'PeopleController@store');
+Route::post('/people/', 'PeopleController@store/');
 Route::get('/people/{person}', 'PeopleController@show');
+Route::put('/people/{person}', 'PeopleController@update');
