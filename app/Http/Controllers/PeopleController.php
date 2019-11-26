4d8093a03043f4931171b4ffb6c2e85ec66fbe9d
Hazem Khalil <hazem.ibrahim.khalil@gmail.com>
2019-11-26 21:50:37 +0200

    Creating the base for making RestfullAPI CRUD

---
+++ app/Http/Controllers/PeopleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PeopleController extends Controller
+{
+    public function show()
+    {
+        $person = [
+            'name' => 'hazem',
+            'email' => 'khalil'
+        ];
+        return $person;
+    }
+}
